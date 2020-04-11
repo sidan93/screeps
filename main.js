4c8b745106b5cc82aa2da637ae4757442e96606b
Sidorov Andrey <sidan93@gmail.com>
2020-04-11 17:45:30 +0700

    Что-то там где то там)

--- main.js
+++ main.js
@@ -1,13 +1,2 @@
-let GameLoop = require('GameLoop');
-let CreepHandlers = require('CreepHandlers');
-let MRoom = require('MRoom');
-
-console.log('new tick');
-
-GameLoop.tick();
-let m_room = new MRoom(Game.rooms[Object.keys(Game.rooms)[0]]);
-// m_room.activate_safe_mode();
-m_room.spawn_creeps();
-CreepHandlers.get_all_creep().forEach(function(creep) {
-  creep.do_action(m_room);
-});
+console.log(1);
+//# sourceMappingURL=main.js.map
